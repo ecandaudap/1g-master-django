18384c4d04520d00b42d23ed3e4b400bdd44fcfb
ecandaudap <candaudap@gmail.com>
2023-09-01 22:11:27 -0600

    branch for clase5

--- bootcamp/models.py
+++ bootcamp/models.py
@@ -7,6 +7,34 @@ from django.db import models
 # Cada modelo representa una tabla de SQL
 # Cada propiedad de la clase(modelo) representa un atributo de la tabla
 
+# La foreign Key se pone en la N en las relaciones 1 - N
+# Cuando hay N - N la FK se pone en la de menors jerarquía
+# 1 generation - N koders
+# N mentors - N generations
+#1 bootcamp - N generations
+
+class Bootcamp(models.Model):
+    '''Bootcamp Model.'''
+
+    name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+            return f"{self.name}"
+
+class Generation(models.Model):
+    '''Generation Model.'''
+
+    number = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# Foreign keys
+    bootcamp = models.ForeignKey(Bootcamp, models.PROTECT, related_name="generations")
+
+
+    def __str__(self):
+        return f"{self.number} {self.bootcamp.name}"
+
 class Koder(models.Model):
     '''Koder Model.'''
     
@@ -17,29 +45,39 @@ class Koder(models.Model):
         ("finished", "Finished")
     ]
 
-    SIZES = [
-        ("s", "Small"), 
-        ("m", "Medium"),
-        ("l", "Large")
-    ]
-
-
-
+   
     first_name = models.CharField(max_length=255) # -> string
     last_name = models.CharField(max_length=255) # -> string
-    generation = models.CharField(max_length=20) # -> string
     email = models.EmailField(unique=True) # -> email único
     phone = models.CharField(max_length=25) # -> núm telefónico
     status = models.CharField(max_length=8, choices= STATUSES, default="active")
-    size = models.CharField(max_length=1, choices= SIZES, default="s")
-    birthdate = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
-    # -> En cuanto se cree nos agrega la hora por automático
-    updated_at = models.DateTimeField(blank=True, null=True)
-    # -> Puede ser nulo a nivel django y nivel base de datos
+    # -> En cuanto se crea nos agrega la hora por automático
+
+# Foreign keys
+    generation = models.ForeignKey(Generation, models.PROTECT, related_name="koders")
 
 # Representar como nos regresan el koder
     def __str__(self):
         return f"FirstName -> {self.first_name}, LastName -> {self.last_name}"
+
+
+class Mentor(models.Model):
+    '''Mentor Model.'''
+
+
+    first_name = models.CharField(max_length=255) # -> string
+    last_name = models.CharField(max_length=255) # -> string
+    email = models.EmailField(unique=True) # -> email único
+    phone = models.CharField(max_length=25) # -> núm telefónico
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# Foreign keys
+    generations = models.ManyToManyField(Generation, related_name="mentors")
+
+    def __str__(self):
+            return f"id: {self.pk} {self.first_name} {self.last_name}"
+
+
 
 
--- bootcamp/views.py
+++ bootcamp/views.py
@@ -5,29 +5,20 @@ from django.template import loader
 
 # Create your views here.
 
+from bootcamp.models import Koder
+
 def get_koder(request, id):
-    koders = [
-        {"id": 1, "name": "Benjamin", "generation": "ig", "bootcamp": "Python"},
-        {"id": 2, "name": "Luis", "generation": "ig", "bootcamp": "Python"}
-    ]
+    koders = Koder.objects.get(pk=id)
 
-    founded_koder  = [koder for koder in koders if koder.id == id]
-    print("Founded koder --->>>", founded_koder[0])
+    return HttpResponse(f"Founded koder --->>> {koders}")
 
-    return HttpResponse(f"Founded koder --->>> {founded_koder}")
+from bootcamp.models import Koder
+
 
 def list_koders(request):
-    context = {
-        "bootcamp":{"name": "Python", "module": "Django"},
-        "koders" : [
-            {"name": "Benjamin", "generation": "ig", "bootcamp": "Python", "is_active": True},
-            {"name": "Luis", "generation": "ig", "bootcamp": "Python", "is_active": True},
-            {"name": "Enrique", "generation": "ig", "bootcamp": "Python", "is_active": False}
-        ],
-    }
+    koders = Koder.objects.all()
 
-    template = loader.get_template("templates/list_koders.html")
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(koders)
 
 def list_mentors(request):
     context = {
